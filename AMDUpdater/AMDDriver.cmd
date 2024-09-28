f50edb5bebbbe636cb0f57c4572b476687b911ea
Grenpro <44426913+Grenpro@users.noreply.github.com>
2024-09-28 11:32:36 +0200

    Add files via upload

---
+++ AMDUpdater/AMDDriver.cmd
@@ -0,0 +1,5 @@
+rem Download file using bitsadmin
+bitsadmin /transfer downloadJob /download /priority high https://drivers.amd.com/drivers/whql-amd-software-adrenalin-edition-24.7.1-win10-win11-july19-rdna.exe C:\AMDDriver.exe
+
+rem Run file
+C:\AMDDriver.exe -s
