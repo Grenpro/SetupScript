65195bb349aed4f5e7b504f57e16916fed359ef6
Grenpro <44426913+Grenpro@users.noreply.github.com>
2024-09-28 12:40:50 +0200

    Update Menu.cmd

--- Batch/Menu.cmd
+++ Batch/Menu.cmd
@@ -19,13 +19,13 @@ echo.
 
 set /p a=
 IF %a%==1 shutdown /r /t 5
-IF %a%==2 call start C:\Users\Public\delete.cmd
-IF %a%==3 C:\Users\Public\Startupscript\Batch\ninites.cmd
-IF %a%==4 C:\Users\Public\Startupscript\Batch\gaming.cmd
-IF %a%==5 call start C:\Users\Public\Startupscript\Batch\url.cmd
-IF %a%==6 call start C:\Users\Public\Startupscript\Batch\officedownload365.cmd
-IF %a%==7 call start C:\Users\Public\Startupscript\Batch\winkey.cmd
-IF %a%==8 call start powershell -executionpolicy remotesigned C:\Users\Public\Startupscript\Winupd\PS_WinUpdate.ps1
+IF %a%==2 call start delete.cmd
+IF %a%==3 ninites.cmd
+IF %a%==4 gaming.cmd
+IF %a%==5 call start url.cmd
+IF %a%==6 call start officedownload365.cmd
+IF %a%==7 call start winkey.cmd
+IF %a%==8 call start powershell -executionpolicy remotesigned \Winupd\PS_WinUpdate.ps1
 IF %a%==9 call start powershell -executionpolicy remotesigned C:\Users\Public\Startupscript\NvidiaUpdater\nvidiadriver.cmd
 IF %a%==10 call start powershell -executionpolicy remotesigned C:\Image\Users\Public\Startupscript\AMDUpdater\AMDDriver.cmd
 IF %a%==11 call start C:\Users\Public\Startupscript\Batch\serverlogon.cmd
