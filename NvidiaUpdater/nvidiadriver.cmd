747871e2d7060c4976ea5b7ea155cfb7d5299161
Grenpro <44426913+Grenpro@users.noreply.github.com>
2024-09-28 11:30:09 +0200

    Add files via upload

---
+++ NvidiaUpdater/nvidiadriver.cmd
@@ -0,0 +1,5 @@
+rem Download file using bitsadmin
+bitsadmin /transfer downloadJob /download /priority high https://us.download.nvidia.com/nvapp/client/10.0.1.256/NVIDIA_app_beta_v10.0.1.256.exe C:\NvidiaDriver.exe
+
+rem Run file
+C:\NvidiaDriver.exe -s
