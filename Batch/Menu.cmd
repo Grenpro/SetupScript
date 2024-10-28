29a735b6a962c4aa42e1f03d6bfaef71c9f23e63
Grenpro <44426913+Grenpro@users.noreply.github.com>
2024-10-28 14:47:29 +0100

    Update Menu.cmd

--- Batch/Menu.cmd
+++ Batch/Menu.cmd
@@ -5,30 +5,28 @@ color 1F
 
 echo.
 echo 1.Restart
-echo 2.Delete Installation
-echo 3.Ninite Installers
-echo 4.Gaming Setups
-echo 5.Mcafee and Cloud Link
-echo 6.Office 365 Installer
-echo 7.Auto Activator (CHECK THIS ON S MODE PC!!!)
-echo 8.Windows Update
-echo 9.Nvidia Driver Update
-echo 10.Amd Driver Update
-echo 11.Mcafee and Cloud Automation
+echo 2.Mcafee and Cloud Link
+echo 3.Office 365 Installer
+echo 4.Auto Activator (CHECK THIS ON S MODE PC!!!)
+echo 5.Windows Update
+echo 6.Nvidia Driver Update
+echo 7.Amd Driver Update
+echo 8.Mcafee and Cloud Automation
+echo 9.SuperCleanerv5
+echo 10.Adwcleaner
 echo.
 
 set /p a=
 IF %a%==1 shutdown /r /t 5
-IF %a%==2 call start delete.cmd
-IF %a%==3 ninites.cmd
-IF %a%==4 gaming.cmd
-IF %a%==5 call start url.cmd
-IF %a%==6 call start officedownload365.cmd
-IF %a%==7 call start winkey.cmd
-IF %a%==8 call start powershell -executionpolicy remotesigned ..\Winupd\PS_WinUpdate.ps1
-IF %a%==9 call start powershell -executionpolicy remotesigned ..\NvidiaUpdater\nvidiadriver.cmd
-IF %a%==10 call start powershell -executionpolicy remotesigned ..\AMDUpdater\AMDDriver.cmd
-IF %a%==11 call start serverlogon.cmd
+IF %a%==2 call start url.cmd
+IF %a%==3 call start officedownload365.cmd
+IF %a%==4 call start winkey.cmd
+IF %a%==5 call start powershell -executionpolicy remotesigned ..\Winupd\PS_WinUpdate.ps1
+IF %a%==6 call start powershell -executionpolicy remotesigned ..\NvidiaUpdater\nvidiadriver.cmd
+IF %a%==7 call start powershell -executionpolicy remotesigned ..\AMDUpdater\AMDDriver.cmd
+IF %a%==8 call start serverlogon.cmd
+IF %a%==9 call start SuperCleanerV5.cmd
+IF %a%==10 call start adwcleaner.cmd
 
 
 test&cls
