37dd21cb224c22f1f1c78b0f66f82d6664aef624
Grenpro <44426913+Grenpro@users.noreply.github.com>
2024-10-30 09:17:29 +0100

    Update Startscript.cmd

--- Batch/Startscript.cmd
+++ Batch/Startscript.cmd
@@ -10,13 +10,13 @@ ping www.microsoft.com || goto :repeat
 
 timeout /T 5
 
-reg import C:\Users\Public\Startupscript\RegistryImport\AddOem.reg
+reg import ..\RegistryImport\AddOem.reg
 
-call start C:\Users\Public\Startupscript\Winre\removewinre2.cmd
+call start ..\Winre\removewinre2.cmd
 
-call start C:\Users\Public\Startupscript\Ninites\package.exe
+call start ..\Ninites\package.exe
 
-call start C:\Users\Public\Startupscript\Batch\Retimer.cmd 
+call start Retimer.cmd 
 
 timeout /T 5
 
@@ -24,24 +24,22 @@ taskkill /F /IM msedge.exe
 
 timeout /T 3
 
-call start C:\Users\Public\Startupscript\Winupd\powershellupd.cmd
+call start ..\Winupd\powershellupd.cmd
 
-call start C:\Users\Public\Startupscript\Batch\power.cmd
+call start power.cmd
 
-call start C:\Users\Public\Startupscript\Batch\setdns.cmd
+call start setdns.cmd
 
-call start C:\Users\Public\Startupscript\Batch\turnoffbde.cmd
+call start turnoffbde.cmd
 
-call start C:\Users\Public\Startupscript\Batch\winkey.cmd
+call start winkey.cmd
 
 timeout /T 10
 
 @echo off
-@echo off F | xcopy C:\Users\Public\Startupscript\Xcopy\startup2.lnk "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\startup2.lnk" /E /H /K /X /Y
+@echo off F | xcopy ..\Xcopy\startup2.lnk "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\startup2.lnk" /E /H /K /X /Y
 
-call start C:\Users\Public\Startupscript\Batch\ChromeLink.cmd
-
-call start powershell -executionpolicy remotesigned C:\Image\Users\Public\Startupscript\ChromeDefault\chrome.ps1
+call start ChromeLink.cmd
 
 exit
 
