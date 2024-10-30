b3efabc2422587f87ed680f61a21c03e6c332364
Grenpro <44426913+Grenpro@users.noreply.github.com>
2024-10-30 11:01:43 +0100

    Update Retimer.cmd

--- Batch/Retimer.cmd
+++ Batch/Retimer.cmd
@@ -4,8 +4,8 @@ color 0A
 Choice /C RNY /N /T 600 /D R /M "Press Y to Restart, N to Cancel"
 
 IF ERRORLEVEL 3 shutdown /r /t 10
-IF ERRORLEVEL 2 C:\Users\Public\Startupscript\Batch\menu.cmd
-IF ERRORLEVEL 1 C:\Users\Public\Startupscript\Batch\restartpc.cmd
+IF ERRORLEVEL 2 menu.cmd
+IF ERRORLEVEL 1 restartpc.cmd
 
 
 
