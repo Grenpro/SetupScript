57e78ca4c9e45140d36dbce10bd77ae84a3969ac
Grenpro <44426913+Grenpro@users.noreply.github.com>
2024-10-30 11:03:29 +0100

    Update chromedefault.ps1

--- Batch/chromedefault.ps1
+++ Batch/chromedefault.ps1
@@ -1,5 +1,5 @@
 # Path to XML file with default app associations
-$xmlPath = "C:\Users\Public\Startupscript\batch\defaultassociations.xml"
+$xmlPath = "defaultassociations.xml"
 
 # Apply the default app associations
 Dism /Online /Import-DefaultAppAssociations:$xmlPath
