7c9ed304ada3e9236e69393e5c78d171d08313d5
Grenpro <44426913+Grenpro@users.noreply.github.com>
2024-10-31 10:02:05 +0100

    Update Programs.cmd

--- Batch/Programs.cmd
+++ Batch/Programs.cmd
@@ -14,7 +14,7 @@ set /p choices=Enter choices separated by space (e.g., 1 3 5):
 for %%a in (%choices%) do (
     IF %%a==1 call start Menu.cmd
     IF %%a==2 call start powershell -executionpolicy remotesigned gamelaunchers.ps1
-    IF %%a==3 call start ..\Winget\browser.cmd
+    IF %%a==3 call start ..\Winget\browser.bat
 
 )
 
