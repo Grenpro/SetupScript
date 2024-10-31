0004c5f8764e8a9226f46bb7e7438a5563168392
Grenpro <44426913+Grenpro@users.noreply.github.com>
2024-10-31 10:10:27 +0100

    Update Programs.cmd

--- Batch/Programs.cmd
+++ Batch/Programs.cmd
@@ -7,6 +7,12 @@ echo.
 echo 1. Previous/Back
 echo 2. Game Launchers (Epic Games, Steam, EA App, Ubisoft Connect)
 echo 3. Browsers (Chrome & Firefox)
+echo 4. OpenOffice
+echo 5. LibreOffice
+echo 6. Discord
+echo 7. VLC
+echo 8. Steam
+echo 9. Epic Games
 echo.
 
 set /p choices=Enter choices separated by space (e.g., 1 3 5): 
@@ -15,7 +21,12 @@ for %%a in (%choices%) do (
     IF %%a==1 call start Menu.cmd
     IF %%a==2 call start powershell -executionpolicy remotesigned gamelaunchers.ps1
     IF %%a==3 call start ..\Winget\browser.bat
-
+    IF %%a==4 winget install --id Apache.OpenOffice -e --silent
+    IF %%a==5 winget install --id TheDocumentFoundation.LibreOffice -e --silent
+    IF %%a==6 winget install --id Discord.Discord -e --silent
+    IF %%a==7 winget install --id VideoLAN.VLC -e --silent
+    IF %%a==8 winget install --id Valve.Steam -e --silent
+    IF %%a==9 winget install --id EpicGames.EpicGamesLauncher -e --silent
 )
 
 test&cls
