bb53c6f2e981966c1751ebfc14ec0331f6ef6e1b
Grenpro <44426913+Grenpro@users.noreply.github.com>
2024-11-01 10:00:53 +0100

    Update browser.cmd

--- Winget/browser.cmd
+++ Winget/browser.cmd
@@ -1,8 +1,8 @@
 @echo off
 echo Installing Google Chrome and Mozilla Firefox...
 
-winget install --id Google.Chrome -e --silent
-winget install --id Mozilla.Firefox -e --silent
+winget install --id Google.Chrome -e --silent --accept-package-agreements --accept-source-agreements
+winget install --id Mozilla.Firefox -e --silent --accept-package-agreements --accept-source-agreements
 
 echo Installation complete.
 exit
