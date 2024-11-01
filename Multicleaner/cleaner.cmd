3ef82cd1fe80f590e7f996d76be90d5ff5a418a9
Grenpro <44426913+Grenpro@users.noreply.github.com>
2024-11-01 13:52:27 +0100

    Update cleaner.cmd

--- Multicleaner/cleaner.cmd
+++ Multicleaner/cleaner.cmd
@@ -1,10 +1,3 @@
-echo System file check complete.
-
-echo Running disk cleanup, please wait...
-echo Running Disk Cleanup... >> "%LOGFILE%"
-
 rem Run Disk Cleanup silently
 cleanmgr.exe /sagerun:1
-echo Disk cleanup completed. >> "%LOGFILE%"
-
-echo Disk cleanup complete.
+exit
