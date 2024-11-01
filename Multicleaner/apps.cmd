db17528407e3d3e17c09df43ea3f2a074c4e70ed
Grenpro <44426913+Grenpro@users.noreply.github.com>
2024-11-01 13:51:54 +0100

    Update apps.cmd

--- Multicleaner/apps.cmd
+++ Multicleaner/apps.cmd
@@ -1,10 +1,3 @@
-rem Check if winget is available and update apps if so
-winget --version >nul 2>&1
-if %errorlevel% neq 0 (
-    echo Winget not available, skipping app updates. >> "%LOGFILE%"
-) else (
-    winget update --all
-    echo App updates completed. >> "%LOGFILE%"
-)
+winget update --all
 
-echo App update complete.
+exit
