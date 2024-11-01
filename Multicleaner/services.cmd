d48dbc9c6fc0418f36e1fa3befeda89151601889
Grenpro <44426913+Grenpro@users.noreply.github.com>
2024-11-01 13:56:50 +0100

    Update services.cmd

--- Multicleaner/services.cmd
+++ Multicleaner/services.cmd
@@ -1,6 +1,3 @@
-echo Resetting Windows update components, please wait...
-echo Resetting Windows Update components... >> "%LOGFILE%"
-
 rem Reset Windows Update components
 net stop wuauserv
 net stop cryptSvc
@@ -12,4 +9,4 @@ net start wuauserv
 net start cryptSvc
 net start bits
 net start msiserver
-echo Windows update component reset complete. >> "%LOGFILE%"
+exit
