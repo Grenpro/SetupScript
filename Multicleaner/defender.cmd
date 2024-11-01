03125a0b1adbb77a0693f687461056d663e4c7f7
Grenpro <44426913+Grenpro@users.noreply.github.com>
2024-11-01 13:55:26 +0100

    Update defender.cmd

--- Multicleaner/defender.cmd
+++ Multicleaner/defender.cmd
@@ -1,16 +1,3 @@
-echo Running malware scan, please wait...
-echo Running malware scan... >> "%LOGFILE%"
+"%ProgramFiles%\Windows Defender\mpcmdrun.exe" -scan
 
-rem Run Windows Defender scan if available
-if exist "%ProgramFiles%\Windows Defender\mpcmdrun.exe" (
-    "%ProgramFiles%\Windows Defender\mpcmdrun.exe" -scan
-    if %errorlevel% neq 0 (
-        echo Windows Defender scan encountered an error. >> "%LOGFILE%"
-    ) else (
-        echo Malware scan completed successfully. >> "%LOGFILE%"
-    )
-) else (
-    echo Windows Defender not found, skipping malware scan. >> "%LOGFILE%"
-)
-
-echo Scan complete.
+exit
