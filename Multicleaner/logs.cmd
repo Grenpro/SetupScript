a6578c2f9893b428d088bdb3cfdd7eed0b6fd5ca
Grenpro <44426913+Grenpro@users.noreply.github.com>
2024-11-01 13:56:26 +0100

    Update logs.cmd

--- Multicleaner/logs.cmd
+++ Multicleaner/logs.cmd
@@ -1,6 +1,4 @@
-echo Clearing event logs...
-echo Clearing event logs... >> "%LOGFILE%"
 for /F "tokens=*" %%G in ('wevtutil el') DO (
     wevtutil cl "%%G"
 )
-echo Event logs cleared. >> "%LOGFILE%"
+exit
