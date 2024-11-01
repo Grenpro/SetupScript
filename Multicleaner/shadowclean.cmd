0bda10d3c1b299246c7e3bc902a6c3ba93a21fb9
Grenpro <44426913+Grenpro@users.noreply.github.com>
2024-11-01 13:57:03 +0100

    Update shadowclean.cmd

--- Multicleaner/shadowclean.cmd
+++ Multicleaner/shadowclean.cmd
@@ -1,4 +1,3 @@
-echo Clearing all but the most recent system restore point...
-echo Clearing system restore points... >> "%LOGFILE%"
+
 vssadmin delete shadows /for=c: /oldest
-echo System restore points cleaned. >> "%LOGFILE%"
+exit
