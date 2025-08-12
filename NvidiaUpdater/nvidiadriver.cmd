334805eaa99b8816020e5ce46c073bd73383192b
Grenpro <44426913+Grenpro@users.noreply.github.com>
2025-08-12 16:07:23 +0200

    Update nvidiadriver.cmd

--- NvidiaUpdater/nvidiadriver.cmd
+++ NvidiaUpdater/nvidiadriver.cmd
@@ -1,5 +1,6 @@
 rem Download file using bitsadmin
-bitsadmin /transfer downloadJob /download /priority high https://us.download.nvidia.com/nvapp/client/10.0.3.163/NVIDIA_app_beta_v10.0.3.163.exe C:\NvidiaDriver.exe
+bitsadmin /transfer downloadJob /download /priority high https://us.download.nvidia.com/nvapp/client/11.0.4.526/NVIDIA_app_v11.0.4.526.exe C:\NvidiaDriver.exe
 
 rem Run file
 C:\NvidiaDriver.exe -s
+
