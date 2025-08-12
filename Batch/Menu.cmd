a3666781fdcb1037fd3dc5a81ad8f67f741768fd
Grenpro <44426913+Grenpro@users.noreply.github.com>
2025-08-12 17:17:33 +0200

    Update Menu.cmd

--- Batch/Menu.cmd
+++ Batch/Menu.cmd
@@ -10,12 +10,11 @@ echo 3. Auto Activator (CHECK THIS ON S MODE PC!!!)
 echo 4. Windows Update
 echo 5. Nvidia Driver Update
 echo 6. AMD Driver Update
-echo 7. Mcafee and Cloud Automation
-echo 8. SuperCleanerv5
-echo 9. Adwcleaner
-echo 10. Startscript
-echo 11. Programs
-echo 12. Multicleaner
+echo 7. SuperCleanerv5
+echo 8. Adwcleaner
+echo 9. Startscript
+echo 10. Programs
+echo 11. Multicleaner
 echo.
 
 set /p choices=Enter choices separated by space (e.g., 1 3 5): 
@@ -27,16 +26,16 @@ for %%a in (%choices%) do (
     IF %%a==4 call start powershell -executionpolicy remotesigned ..\Winupd\PS_WinUpdate.ps1
     IF %%a==5 call start powershell -executionpolicy remotesigned ..\NvidiaUpdater\nvidiadriver.cmd
     IF %%a==6 call start powershell -executionpolicy remotesigned ..\AMDUpdater\AMDDriver.cmd
-    IF %%a==7 call start serverlogon.cmd
-    IF %%a==8 call start SuperCleanerV5.cmd
-    IF %%a==9 call start adwclean.cmd
-    IF %%a==10 call start Startscript.cmd
-    IF %%a==11 Programs.cmd
-    IF %%a==12 call start ..\Multicleaner\Multiclean.cmd
+    IF %%a==7 call start SuperCleanerV5.cmd
+    IF %%a==8 call start adwclean.cmd
+    IF %%a==9 call start Startscript.cmd
+    IF %%a==10 Programs.cmd
+    IF %%a==11 call start ..\Multicleaner\Multiclean.cmd
 )
 
 test&cls
 GOTO Start
+
 
 
 
