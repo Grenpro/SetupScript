73112565223b6c1a5ed065b201927a07bfc8bd71
Grenpro <44426913+Grenpro@users.noreply.github.com>
2025-08-12 16:00:35 +0200

    Update Menu.cmd

--- Batch/Menu.cmd
+++ Batch/Menu.cmd
@@ -4,40 +4,39 @@ test&cls
 color 1F
 
 echo.
-echo 1. Self Destruction
-echo 2. Mcafee and Cloud Link
-echo 3. Office 365 Installer
-echo 4. Auto Activator (CHECK THIS ON S MODE PC!!!)
-echo 5. Windows Update
-echo 6. Nvidia Driver Update
-echo 7. AMD Driver Update
-echo 8. Mcafee and Cloud Automation
-echo 9. SuperCleanerv5
-echo 10. Adwcleaner
-echo 11. Startscript
-echo 12. Programs
-echo 13. Multicleaner
+echo 1. Mcafee and Cloud Link
+echo 2. Office 365 Installer
+echo 3. Auto Activator (CHECK THIS ON S MODE PC!!!)
+echo 4. Windows Update
+echo 5. Nvidia Driver Update
+echo 6. AMD Driver Update
+echo 7. Mcafee and Cloud Automation
+echo 8. SuperCleanerv5
+echo 9. Adwcleaner
+echo 10. Startscript
+echo 11. Programs
+echo 12. Multicleaner
 echo.
 
 set /p choices=Enter choices separated by space (e.g., 1 3 5): 
 
 for %%a in (%choices%) do (
-    IF %%a==1 call start delete.cmd
-    IF %%a==2 call start url.cmd
-    IF %%a==3 call start officedownload365.cmd
-    IF %%a==4 call start winkey.cmd
-    IF %%a==5 call start powershell -executionpolicy remotesigned ..\Winupd\PS_WinUpdate.ps1
-    IF %%a==6 call start powershell -executionpolicy remotesigned ..\NvidiaUpdater\nvidiadriver.cmd
-    IF %%a==7 call start powershell -executionpolicy remotesigned ..\AMDUpdater\AMDDriver.cmd
-    IF %%a==8 call start serverlogon.cmd
-    IF %%a==9 call start SuperCleanerV5.cmd
-    IF %%a==10 call start adwclean.cmd
-    IF %%a==11 call start Startscript.cmd
-    IF %%a==12 Programs.cmd
-    IF %%a==13 call start ..\Multicleaner\Multiclean.cmd
+    IF %%a==1 call start url.cmd
+    IF %%a==2 call start officedownload365.cmd
+    IF %%a==3 call start winkey.cmd
+    IF %%a==4 call start powershell -executionpolicy remotesigned ..\Winupd\PS_WinUpdate.ps1
+    IF %%a==5 call start powershell -executionpolicy remotesigned ..\NvidiaUpdater\nvidiadriver.cmd
+    IF %%a==6 call start powershell -executionpolicy remotesigned ..\AMDUpdater\AMDDriver.cmd
+    IF %%a==7 call start serverlogon.cmd
+    IF %%a==8 call start SuperCleanerV5.cmd
+    IF %%a==9 call start adwclean.cmd
+    IF %%a==10 call start Startscript.cmd
+    IF %%a==11 Programs.cmd
+    IF %%a==12 call start ..\Multicleaner\Multiclean.cmd
 )
 
 test&cls
 GOTO Start
+
 
 
