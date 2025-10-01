ac2cf45425047b9ec76235aa04931adbeda46421
Grenpro <44426913+Grenpro@users.noreply.github.com>
2025-10-01 13:35:49 +0200

    Update jottaauto.cmd

--- Batch/jottaauto.cmd
+++ Batch/jottaauto.cmd
@@ -1,3 +1,3 @@
 @echo off
-powershell -Command "Invoke-WebRequest -Uri https://sw.jotta.cloud/autoinstaller/desktop-auto-installer.exe -OutFile $env:TEMP\jotta-installer.exe" && "%TEMP%\jotta-installer.exe" /S && del "%TEMP%\jotta-installer.exe"
+powershell -Command "Invoke-WebRequest -Uri https://sw.jotta.cloud/autoinstaller/desktop-auto-installer.exe -OutFile $env:TEMP\jotta-installer.exe" && "%TEMP%\jotta-installer.exe" && del "%TEMP%\jotta-installer.exe"
 exit
