67ba0a2f710c40a4d8715d4114dfa6e825354641
Grenpro <44426913+Grenpro@users.noreply.github.com>
2025-10-01 13:19:52 +0200

    Update Callingcard.cmd

--- Batch/Callingcard.cmd
+++ Batch/Callingcard.cmd
@@ -1,2 +1,3 @@
 @echo off
 powershell -Command "Invoke-WebRequest -Uri https://elkitsupportstorage.blob.core.windows.net/supportagreement/Elkjop_CC.msi -OutFile $env:TEMP\Elkjop_CC.msi" && msiexec /i "%TEMP%\Elkjop_CC.msi" /qn /norestart && del "%TEMP%\Elkjop_CC.msi"
+exit
